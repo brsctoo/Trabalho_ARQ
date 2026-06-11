ff48267d35f40b2238fdf0edbf3dbe348b15f9a9
feagomes <felipegomes1302@gmail.com>
2026-06-11 18:28:13 -0300

    criei arquivo pra instrucoes

---
+++ instrucoes.py
@@ -0,0 +1,23 @@
+memoria_ram = [0] * 256
+
+registradores = {
+    'A': 0, 'B': 0,
+    'PC': 0, 'IR': '',
+    'MAR': 0, 'MBR': 0,
+    'AC': 0, 'M': 0, 'R': 0,
+    'C': 0, 'N': 0, 'Z': 0
+}
+
+def executar_load(endereco):
+    
+def executar_store(endereco):
+
+def executar_add(endereco):
+
+def executar_sub(endereco):
+    
+def executar_mult(endereco):
+
+def executar_jump(endereco):
+
+def executar_jump_positivo(endereco):
