0dea00b8f55b590b98c083b71a8a3bdc68447f0b
feagomes <felipegomes1302@gmail.com>
2026-06-11 19:01:55 -0300

    completei as instrucoes

--- instrucoes.py
+++ instrucoes.py
@@ -9,15 +9,36 @@ registradores = {
 }
 
 def executar_load(endereco):
+    registradores["AC"] = memoria_ram[endereco]
     
 def executar_store(endereco):
+    memoria_ram[endereco] = registradores["AC"]
 
 def executar_add(endereco):
+    registradores["AC"] += memoria_ram[endereco]
 
 def executar_sub(endereco):
+    registradores["AC"] -= memoria_ram[endereco]
     
 def executar_mult(endereco):
+    registradores["AC"] *= memoria_ram[endereco]
 
 def executar_jump(endereco):
+    registradores["PC"] = endereco
 
 def executar_jump_positivo(endereco):
+    if registradores["AC"] > 0:
+        registradores["PC"] = endereco
+
+def executar_move(destino, origem):
+    registradores[destino] = registradores[origem]
+
+def executar_loadI(endereco):
+    registradores["MAR"] = endereco
+    registradores["MBR"] = memoria_ram[registradores["MAR"]]
+    registradores["AC"] = memoria_ram[registradores["MBR"]]
+
+def executar_storeI(endereco):
+    registradores["MAR"] = endereco
+    registradores["MBR"] = memoria_ram[registradores["MAR"]]
+    memoria_ram[registradores["MBR"]] = registradores["AC"]
